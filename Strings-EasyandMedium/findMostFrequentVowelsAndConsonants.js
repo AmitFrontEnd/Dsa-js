01f6e6cb7563a357b969fa7de8bab48d010dd493
Amit Kumar <amitemailu@gmail.com>
2026-09-02 13:25:31 +0530

    solve: find most frequent vowel and consonant using brute force approach

---
+++ Strings-EasyandMedium/findMostFrequentVowelsAndConsonants.js
@@ -0,0 +1,23 @@
+const maxFreqSumBrute = (s) => {
+  let maxVowels = 0, maxConsonant = 0
+  for (i = 0; i < s.length; i++) {
+    if (s[i] !== 'a' && s[i] !== 'e' && s[i] !== 'i' && s[i] !== 'o' && s[i] !== 'u') continue;
+    let vowelsCount = 0
+    for (let j = 0; j < s.length; j++) {
+      if (s[j] === s[i])
+        vowelsCount++
+    }
+    maxVowels = maxVowels < vowelsCount ? vowelsCount : maxVowels
+  }
+
+  for (i = 0; i < s.length; i++) {
+    if (s[i] === 'a' || s[i] === 'e' || s[i] === 'i' || s[i] === 'o' || s[i] === 'u') continue;
+    let consonantCount = 0
+    for (let j = 0; j < s.length; j++) {
+      if (s[j] === s[i])
+        consonantCount++
+    }
+    maxConsonant = maxConsonant < consonantCount ? consonantCount : maxConsonant
+  }
+  return maxVowels + maxConsonant
+};
