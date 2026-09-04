c7fce27192e2d53dd900b46748ffa1e97d417d9d
Amit Kumar <amitemailu@gmail.com>
2026-09-04 12:16:18 +0530

    solve: palindrome problem using brute approach

---
+++ Strings-EasyandMedium/validPalindrome.js
@@ -0,0 +1,24 @@
+
+const isPalindromeBrute = s => {
+  let alphaRegex = /[a-zA-Z0-9]/
+  let alphaString = ""
+  for (let char of s) {
+    if (alphaRegex.test(char))
+      alphaString += char.toLowerCase()
+  }
+
+  let reverseAlphaStringArray = alphaString.split("")
+  let left = 0, right = reverseAlphaStringArray.length - 1
+  while (left < right) {
+    let temp = reverseAlphaStringArray[left]
+    reverseAlphaStringArray[left] = reverseAlphaStringArray[right]
+    reverseAlphaStringArray[right] = temp
+    left++
+    right--
+  }
+
+  if (alphaString === reverseAlphaStringArray.join("")) return true
+  return false
+};
+
+console.log(isPalindromeBrute("race a car"))
