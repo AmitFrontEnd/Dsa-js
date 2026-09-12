b1ebf2379b43ad646a6244a6e506c26826f321c6
Amit Kumar <amitemailu@gmail.com>
2026-09-12 13:10:24 +0530

    Optimized time complexity from O(n*m log m) to O(n*m)

--- Strings-EasyandMedium/groupAnagrams.js
+++ Strings-EasyandMedium/groupAnagrams.js
@@ -8,4 +8,22 @@ const groupAnagramsBetter = (strs) => {
   return Object.values(map)
 }
 
-console.log(groupAnagramsBetter(["eat", "tea", "tan", "ate", "nat", "bat"]))
+
+var groupAnagramsOptimal = function (strs) {
+  let map = {}
+  for (let i = 0; i < strs.length; i++) {
+    let arr = new Array(26).fill(0);
+    for (let j = 0; j < strs[i].length; j++) {
+      arr[strs[i][j].charCodeAt() - 97]++
+    }
+    let key = ""
+    for (let k = 0; k < arr.length; k++) {
+      key += String.fromCharCode(97 + k) + arr[k]
+    }
+    if (!map[key]) map[key] = [strs[i]]
+    else map[key].push(strs[i])
+  }
+  return Object.values(map)
+}
+
+console.log(groupAnagramsOptimal(["eat", "tea", "tan", "ate", "nat", "bat"]))
