5b57b2d1a0ab52ed24e983f3b4571b03191db348
Amit Kumar <amitemailu@gmail.com>
2026-09-24 11:47:09 +0530

    solved using optimal approach

--- Stacks_and_Queues/nextGreaterElement.js
+++ Stacks_and_Queues/nextGreaterElement.js
@@ -19,4 +19,31 @@ var nextGreaterElementBrute = function (nums1, nums2) {
   return ans
 }
 
-console.log(nextGreaterElementBrute([1, 3, 5, 2, 4], [6, 5, 4, 3, 2, 1, 7]))
+const nextGreaterElementOptimal = (nums1, nums2) => {
+  let stack = []
+  let ans = []
+  let ngElemMap = {}
+  stack.push(nums2[nums2.length - 1])
+  ngElemMap[nums2[nums2.length - 1]] = -1
+
+  for (let i = nums2.length - 2; i >= 0; i--) {
+    while (stack.length) {
+      let num = stack.pop()
+      if (nums2[i] < num) {
+        ngElemMap[nums2[i]] = num
+        stack.push(num)
+        break
+      }
+    }
+    if (!ngElemMap[nums2[i]]) {
+      ngElemMap[nums2[i]] = -1
+    }
+    stack.push(nums2[i])
+  }
+  for (let i = 0; i < nums1.length; i++) {
+    ans.push(ngElemMap[nums1[i]])
+  }
+  return ans
+}
+
+console.log(nextGreaterElementOptimal([1, 3, 5, 2, 4], [6, 5, 4, 3, 2, 1, 7]))
